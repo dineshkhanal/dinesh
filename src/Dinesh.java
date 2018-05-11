0af02082eaf74a527aa9c21835816ce60249ca75
Dikshyant Rai <drai@Dikshyants-MacBook-Pro.local>
2018-05-11 13:43:17 -0400

    added a function

--- src/Dinesh.java
+++ src/Dinesh.java
@@ -3,5 +3,14 @@ public class Dinesh {
 	public static void main(String[] args) {
 		System.out.println("Dinesh says hello.");
 		System.out.println("K cha bro");
+		System.out.println(Arrays.toString(populateArray(100)));
+	}
+
+	public static void populateArray(int n){
+		int[] toReturn = new int[n+1];
+		for(int i=0; i < n; i++){
+			toReturn[i] = i;
+		}
+		return toReturn;
 	}
 }
